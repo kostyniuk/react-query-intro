b6efdcacc0ecafcc5fcb469355b2ce2a52b576ad
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-09-07 21:26:42 +0200

    migrate to newer approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import { Routes, Route } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import Above from './pages/Above'
 
 function App() {
   return (
     <div>
       <Header />
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/above" element={<Above />} />
-      </Routes>
+      <Outlet />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,39 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import Dashboard from './pages/Dashboard.tsx'
+import Above from './pages/Above.tsx'
 
 const queryClient = new QueryClient()
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true, // This makes it the default route for "/"
+        element: <Home />,
+      },
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "above", 
+        element: <Above />,
+      },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>  
-      <BrowserRouter>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
-      </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
 )

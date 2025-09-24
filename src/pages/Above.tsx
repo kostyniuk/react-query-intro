9b4a63b735064a240867c02cba7839633debaa05
Alex Kostyniuk <kostyniuk7@gmail.com>
2025-09-24 22:49:32 +0200

    i've not posted, but tanstack-query is approved in AMFG /party

--- src/pages/Above.tsx
+++ src/pages/Above.tsx
@@ -5,11 +5,14 @@ export default function Above() {
   const repos = useQuery({
     queryKey: ["repos"],
     queryFn: () => loadRepos(),
-    staleTime: 1000 * 60 * 5, // 5 minutes
+    staleTime: 1000, // 5 minutes
   })
+
+  console.log(repos)
+
   return <>
     <h1>About</h1>
-    {repos.data.map((repo: any) => (
+    {repos?.data?.map((repo: any) => (
       <p key={repo.id}>{repo.name}</p>
     ))}
   </>
